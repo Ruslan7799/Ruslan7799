385d9c62cce09683163c90567b0fcf9b1e1442a8
Ruslan Masharipov <102555505+Ruslan7799@users.noreply.github.com>
2022-03-29 01:58:02 +0500

    Create vue.js

---
+++ vue.js
@@ -0,0 +1,47 @@
+<template>
+     <div>
+       <span v-text="msg"> </span> <br/>
+       <span v-html ="title"> </span>     
+    </div>
+
+    <div v-if ="isAdmin">
+      <h1>Assalomu alaykum, Hurmatli Admistrator</h1>
+    </div>
+     
+     <div v-else>
+      <h1>Assalomu alaykum, Hurmatli Foydalanuvchi</h1>
+    </div>
+
+
+    <div>
+      <p v-if="word === 'car' "> Mashina </p>
+      <p v-else-if="word === 'Book' "> Kitob </p>
+      <p v-else-if="word === 'animal' "> Hayvon </p>
+      <p v-else> Bunday soz mavjud emas </p>
+      <p v-show = "isBool">Men Ko'rinyabman</p>
+    </div>
+
+    <div>
+      <p v-for ="i in 10">Salom</p>
+    </div>
+</template>
+
+<script>
+   export default {
+     name :'App' ,
+     data() {
+         return {
+           msg : "Tezkor Xabar" ,
+           title : "<h1>Salom Dunyo</h1>",
+           isAdmin :true ,
+           word : "" ,
+           isBool : false,
+         }
+      }
+    }
+  
+</script>
+
+<style>
+
+</style>
