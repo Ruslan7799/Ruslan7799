36779ee6980f251a11b334b12758493b50e4a4ed
Ruslan Masharipov <102555505+Ruslan7799@users.noreply.github.com>
2022-04-10 01:33:25 +0500

    Create router.vue

---
+++ router.vue
@@ -0,0 +1,11 @@
+Contact.vue//////
+1) Hello Vue
+Router.js //////
+2)index.js
+ {
+    path:'/contact',
+    name:'contact',
+    component: () => import('../views/Contact.vue')
+  }
+  3)App.vue///////
+  <router-link to="/contact">Contact</router-link>
